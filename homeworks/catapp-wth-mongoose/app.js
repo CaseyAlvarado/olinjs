a6c0f4e2700a0888c1e1a1ec3d8d7a56995f0aac
Casey Alvarado <alvarado.casey@gmail.com>
2016-02-01 01:13:31 -0500

    starting to implement mongoose with new cat app

---
+++ homeworks/catapp-wth-mongoose/app.js
@@ -0,0 +1,27 @@
+var exphbs = require('express-handlebars');
+
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var express = require('express');
+var index = require('./routes/catappindex')
+var app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', index.home);
+app.get('/new', index.newCat); 
+app.get('/cats', index.cats); 
+app.get('/cats/bycolor/:color', index.bycolor); 
+app.get('/cats/delete/old', index.killcat); 
+
+app.listen(3002);
