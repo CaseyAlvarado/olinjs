2b8023a7a0836b47ef15d99fbbc0f2822a5cd083
Casey Alvarado <alvarado.casey@gmail.com>
2016-02-23 19:49:53 -0500

    adding kitchen.js and order.js

---
+++ homeworks/burgerapp/models/orderModel.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose'); 
+var Schema = mongoose.Schema; 
+
+var orderSchema = mongoose.Schema({ 
+	name: String, 
+	price: Number, 
+	ingredients: [{type: Schema.ObjectId, ref: 'ingredients'}], 
+	completed: Boolean
+
+}, {'collection': 'orders' }); 
+
+module.exports = mongoose.model('orders',  orderSchema); 
